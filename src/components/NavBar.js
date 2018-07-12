0144b7897dbe8caf02385dcfab82a7aed2c5876d
giqbal <gohars.iqbal@gmail.com>
2018-07-12 22:42:24 +0100

    Refactored NavBar to downgrade to func. Search functionality moved to SearchBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,58 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserSignIn from './UserSignIn';
-import {Link} from 'react-router-dom'
-import {debounce} from 'lodash';
+import SearchBar from './SearchBar';
 
-class NavBar extends Component {
-    state = {
-        searchInput: '',
-        searchResults: []
-    }
-    render() {
-        const {currentUser, login, logout} = this.props;
-        return (
-            <div>
-                <a href='http://localhost:3000/'><h1>Northcoders News</h1></a>
-                <div>
-                    <input placeholder='🔍 Articles & Topics' id='searchInput' value={this.state.searchInput} onChange={this.handleSearchInput}/>
-                    {this.state.searchResults.map(result => <div key={result._id} onClick={this.clearSearchInput}><Link to={`/${result.itemType}/${result.itemType === 'article'? result._id : result.slug}`}>{`${result.itemType}: ${result.title}`}</Link></div>)}
-                </div>
-                <UserSignIn currentUser={currentUser} login={login} logout={logout} />
-            </div>
-        );
-    }
-
-    handleSearchInput = ({target}) => {
-        this.setState({
-            searchInput: target.value
-        })
-        this.search(target.value);
-    }
-
-    search = debounce(searchTerm => {
-        const {articles, topics} = this.props;
-        const objsToSearch = articles.concat(topics)
-        const searchResults = objsToSearch.reduce((acc, current) => {
-            if (
-                searchTerm.length &&
-                (current.title.toLowerCase().includes(searchTerm.toLowerCase()) || 
-                (current.body && current.body.toLowerCase().includes(searchTerm.toLocaleLowerCase())))
-            ) {
-                acc.push({...current, itemType: current.body? 'article': 'topic'});
-            }
-            return acc;
-        }, [])
-        this.setState({
-            searchResults
-        })
-    }, 500)
-
-    clearSearchInput = () => {
-        this.setState({
-            searchInput: '',
-            searchResults: []
-        })
-    }
+const NavBar = ({currentUser, login, logout, articles, topics}) => {
+    return (
+        <div>
+            <a href='http://localhost:3000/'><h1>Northcoders News</h1></a>
+            <SearchBar articles={articles} topics={topics} />
+            <UserSignIn currentUser={currentUser} login={login} logout={logout} />
+        </div>
+    );
 }
 
 export default NavBar;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import {debounce} from 'lodash';
+
+class SearchBar extends Component {
+    state = {
+        searchInput: '',
+        searchResults: []
+    }
+    render() {
+        return (
+            <div>
+                <input placeholder='🔍 Articles & Topics' id='searchInput' value={this.state.searchInput} onChange={this.handleSearchInput}/>
+                {this.state.searchResults.map(result => <div key={result._id} onClick={this.clearSearchInput}><Link to={`/${result.itemType}/${result.itemType === 'article'? result._id : result.slug}`}>{`${result.itemType}: ${result.title}`}</Link></div>)}
+            </div>
+        );
+    }
+
+    handleSearchInput = ({target}) => {
+        this.setState({
+            searchInput: target.value
+        })
+        this.search(target.value);
+    }
+
+    search = debounce(searchTerm => {
+        const {articles, topics} = this.props;
+        const objsToSearch = articles.concat(topics)
+        const searchResults = objsToSearch.reduce((acc, current) => {
+            if (
+                searchTerm.length &&
+                (current.title.toLowerCase().includes(searchTerm.toLowerCase()) || 
+                (current.body && current.body.toLowerCase().includes(searchTerm.toLocaleLowerCase())))
+            ) {
+                acc.push({...current, itemType: current.body? 'article': 'topic'});
+            }
+            return acc;
+        }, [])
+        this.setState({
+            searchResults
+        })
+    }, 500)
+
+    clearSearchInput = () => {
+        this.setState({
+            searchInput: '',
+            searchResults: []
+        })
+    }
+}
+
+export default SearchBar;
